fa46aca79598a7226a87dd00e960288d75b7c99c
huangxxzn <hxxzn12@gmail.com>
2021-04-06 14:24:06 -0400

    add pseudoflow to algo

--- qore/algorithms/__init__.py
+++ qore/algorithms/__init__.py
@@ -1,2 +1,3 @@
 from .asp import ASP
+from .pseudoflow import Pseudoflow
 from qiskit.aqua.algorithms import QAOA, VQE
---
+++ qore/algorithms/pseudoflow.py
@@ -0,0 +1,36 @@
+""" The pseudoflow algorithm for open-pit mining problems.
+
+See https://hochbaum.ieor.berkeley.edu/html/pub/Hochbaum-OR.pdf
+"""
+
+from typing import Optional, Union, Dict, List, Callable
+import networkx as nx
+from networkx import Graph
+from networkx.classes.digraph import DiGraph
+import pseudoflow
+# import numpy as np
+from qore.utils import single_qubit_pauli, null_operator, measure_operator, get_bitstring_probabilities
+from qore.model.mine import Mine
+
+class Pseudoflow():
+    def __init__(self,
+                 graph: DiGraph,
+                 source: int,
+                 sink: int,
+                 ) -> None:
+        self.graph = graph
+        self.source = source
+        self.sink = sink
+
+    def run(self, verbose=False) -> str:
+        breakpoints, cuts, info = pseudoflow.hpf(
+            self.graph,
+            self.source,
+            self.sink,
+            const_cap='const')
+        if verbose: print(info)
+        ground_state = [value[0] for _, value in cuts.items()][1:-1]
+        bitstring = "".join(list(map(str, ground_state[::-1])))
+        return {'ground_state': bitstring}
+        
+
